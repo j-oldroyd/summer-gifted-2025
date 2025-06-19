c872f35bec6ee07d7d8d913424cf0b66111401f6
Jesse Oldroyd <oldroyd.j@wvwc.edu>
2025-06-19 12:03:26 -0400

    Cleanup Chaos Game file.

--- chaos-animate.py
+++ chaos-animate.py
@@ -3,28 +3,33 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 import math
 import random
-t = 0
-n = 3
-r = 0.75
+
+# Set up initial parameters for THE CHAOS GAME
+n = 3 # number of sides
+r = 0.75 # how far along path
+
+# Draw points on the polygon
 angle = 2*np.pi/n
 pts = [[np.sin(k*angle), np.cos(k*angle)] for k in range(n)]
-# plt.plot([0, 5, 10], [ 0, math.sqrt(75), 0], "ro")
-#plt.plot(pts, "ro")
 
 x, y = zip(*pts)
 fig = plt.figure()
 plt.plot(x, y, "ro")
 
-
-px, py = 0, 0 # starting point
+# Choose a starting point
+px, py = 0, 0
 plt.plot(px, py, "ko")
 
+# From the starting point, start moving towards one of the vertices of
+# the polygon.  Then plot that point, pick another vertex to move
+# towards, and continue
 def animate(frame):
     global px, py # probably a bad idea
     next_direction = np.array(random.choice(pts))
     px = (1 - r)*px + r*next_direction[0]
     py = (1 - r)*py + r*next_direction[1]
-    plt.plot(px, py, "ko", markersize="2")
-    
+    plt.plot(px, py, "bo", markersize="1")
+
+# Animate!
 ani = FuncAnimation(fig, animate, frames=400, interval=10)
 plt.show()
