c77cdbf38c346c33605f77349559f2679361d7ab
Jesse Oldroyd <oldroyd.j@wvwc.edu>
2025-06-22 17:52:11 -0400

    Add chaos game notes and update animation.

--- chaos-animate.py
+++ chaos-animate.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
-import math
 import random
 
 # Set up initial parameters for THE CHAOS GAME
-n = 3 # number of sides
-r = 0.75 # how far along path
+n = 4  # number of sides
+r = .5  # how far along path
 
 # Draw points on the polygon
 angle = 2*np.pi/n
-pts = [[np.sin(k*angle), np.cos(k*angle)] for k in range(n)]
+pts = [[2*np.sin(k*angle), 2*np.cos(k*angle)] for k in range(n)]
 
 x, y = zip(*pts)
 fig = plt.figure()
@@ -20,16 +19,19 @@ plt.plot(x, y, "ro")
 px, py = 0, 0
 plt.plot(px, py, "ko")
 
+
 # From the starting point, start moving towards one of the vertices of
 # the polygon.  Then plot that point, pick another vertex to move
 # towards, and continue
 def animate(frame):
-    global px, py # probably a bad idea
+    """Update Chaos Game animation."""
+    global px, py  # probably a bad idea
     next_direction = np.array(random.choice(pts))
     px = (1 - r)*px + r*next_direction[0]
     py = (1 - r)*py + r*next_direction[1]
     plt.plot(px, py, "bo", markersize="1")
 
 # Animate!
-ani = FuncAnimation(fig, animate, frames=400, interval=10)
+anim = FuncAnimation(fig, animate, frames=1000, interval=1)
+
 plt.show()
